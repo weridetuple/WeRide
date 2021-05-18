43219c107f62f9e49a2ddae16a61bd30b796d5f2
Madison Velasquez <madisonv100@gmaiil.com>
2021-05-18 12:29:13 -0700

    connection string bug

---
+++ WeRide_Backend/WeRide_Backend/Controllers/TestController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WeRide_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private static string Host = "weride-postgres-server.postgres.database.azure.com";
+        private static string User = "WeRide_Corporation@weride-postgres-server";
+        private static string DBname = "postgres";
+        private static string Password = "TaylorSwift2021";
+        private static string Port = "5432";
+        public TestController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string con = "Server=weride-postgres-server.postgres.database.azure.com,1433;Initial Catalog=postgres;Persist Security Info=False;User ID=WeRide_Corporation@weride-postgres-server;Password=TaylorSwift2021;";
+            string query = @"select DummyData from dbo.Test";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString(con);
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(con
+))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ WeRide_Backend/WeRide_Backend/Models/Testcs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeRide_Backend.Models
+{
+    public class Testcs
+    {
+        public string dummyData { get; set; }
+    }
+}
