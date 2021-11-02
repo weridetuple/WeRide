94ee764f46597986d82658b5847f433e5abfeb65
Phong Bach <phong.bach@wsu.edu>
2021-11-02 03:00:31 -0700

    Show user profile picture on nav bar

--- web-app/src/Components/NavBar.tsx
+++ web-app/src/Components/NavBar.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown, Image } from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 export default function NavBar() {
     const {
@@ -25,13 +26,13 @@ export default function NavBar() {
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
                     <Navbar.Brand href="#home">
-                        <img
+                        <Image
                             alt=""
                             src="/assets/brand-only.png"
                             width="30"
                             height="30"
                             className="d-inline-block align-top"
-                        />{' '}
+                        />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
@@ -43,34 +44,49 @@ export default function NavBar() {
                             <Nav.Link href="#pricing">Pricing</Nav.Link>
                         </Nav>
                         <Nav>
-                            <NavDropdown title="Profile" id="collasible-nav-dropdown">
-                                {!isAuthenticated && (
-                                    <>
-                                        <NavDropdown.Item href="#action/signUp"
-                                            onClick={signUpWithRedirect}>
-                                            Sign Up
-                                        </NavDropdown.Item>
-                                        <NavDropdown.Item href="#action/logIn"
-                                            onClick={loginWithRedirect}>
-                                            Log In
-                                        </NavDropdown.Item>
-                                    </>
-                                )}
-                                {isAuthenticated && (
-                                    <>
-                                        <NavDropdown.Item href="#welcome"
-                                            className="disabled">
-                                            Hi, {user!.name}
-                                        </NavDropdown.Item>
-                                        <NavDropdown.Item href="#action/logOut"
-                                            onClick={logoutWithRedirect}>
-                                            Log Out
-                                        </NavDropdown.Item>
-                                    </>
-                                )}
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/help">Help</NavDropdown.Item>
-                            </NavDropdown>
+                            {!isAuthenticated && (
+                                <NavDropdown
+                                    title={
+                                        <FontAwesomeIcon icon={faUser} />
+                                    }
+                                    id="collasible-nav-dropdown"
+                                    className="fas fa-user">
+                                    <NavDropdown.Item href="#action/signUp"
+                                        onClick={signUpWithRedirect}>
+                                        Sign Up
+                                    </NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/logIn"
+                                        onClick={loginWithRedirect}>
+                                        Log In
+                                    </NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item href="#action/help">Help</NavDropdown.Item>
+                                </NavDropdown>
+                            )}
+                            {isAuthenticated && (
+                                <NavDropdown
+                                    title={
+                                        <Image 
+                                            alt=""
+                                            src={user!.picture}
+                                            width="30"
+                                            height="30"
+                                            className="d-inline-block align-top"
+                                            roundedCircle
+                                        />}
+                                    id="collasible-nav-dropdown">
+                                    <NavDropdown.Item href="#profile">
+                                        Profile
+                                    </NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/logOut"
+                                        onClick={logoutWithRedirect}>
+                                        Log Out
+                                    </NavDropdown.Item>
+
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item href="#action/help">Help</NavDropdown.Item>
+                                </NavDropdown>
+                            )}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
